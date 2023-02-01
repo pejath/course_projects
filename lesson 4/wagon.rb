0348598c35f4ad2895a2d08513392d9174fa9c86
pejath <pejath@yandex.ru>
2023-02-02 00:30:04 +0300

    lesson 7 fixes

--- lesson 4/wagon.rb
+++ lesson 4/wagon.rb
@@ -5,11 +5,20 @@ class Wagon
   def initialize(attributes = {})
     @number = rand(1..100)
     @company_name = attributes[:company_name]
+    @full_capacity = attributes[:full_capacity]
+    @occupied_capacity = 0
     validate!
   end
 
   def validate!
-    raise 'Company name can\'t be empty' if @company_name.nil?
+    errors = []
+    errors << 'Company name can\'t be empty' if @company_name.nil?
+    errors << 'Full Capacity name can\'t be empty' if @full_capacity.nil?
+    raise errors.join('. ') unless errors.empty?
+  end
+
+  def empty_capacity
+    @full_capacity - @occupied_capacity
   end
 
   def valid?
